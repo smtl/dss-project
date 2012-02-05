234de17069e583cb2f5b8dd21654ed32eebb6e7f
stephenlowry@gmail.com <stephenlowry@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-02-05 13:58:36 +0000

    Admin working for questions/answers. helloworld example working

--- dss/urls.py
+++ dss/urls.py
@@ -4,14 +4,10 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dss.views.home', name='home'),
-    # url(r'^dss/', include('dss.foo.urls')),
+urlpatterns = patterns('dss.questions.views',
+    url(r'^hello/', 'hello'),
+)
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
