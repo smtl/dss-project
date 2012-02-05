e8d8f52823c2ca7886302e112ed95977cf815d24
stephenlowry@gmail.com <stephenlowry@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-02-05 19:57:10 +0000

    view changes

--- dss/questions/views.py
+++ dss/questions/views.py
@@ -2,6 +2,9 @@
 
 from django.http import HttpResponse
 
-def hello(request):
-    hello = "Hello "+request.GET['name']
+def hello(request, name="world"):
+    if 'name' in request.GET and request.GET['name'] is not None:
+    	hello = "Hello "+request.GET['name']+"!"
+    else:
+        hello = "Hello World!"
     return HttpResponse(hello)
