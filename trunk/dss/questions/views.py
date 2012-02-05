963995ff69fda03ec2d1c6a04dc228f3e46f78b7
stephenlowry@gmail.com <stephenlowry@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-02-05 22:34:20 +0000

    merge slowry 29:60 into trunk

--- trunk/dss/questions/views.py
+++ trunk/dss/questions/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from django.http import HttpResponse
+
+def hello(request, name="world"):
+    if 'name' in request.GET and request.GET['name'] is not None:
+    	hello = "Hello "+request.GET['name']+"!"
+    else:
+        hello = "Hello World!"
+    return HttpResponse(hello)
