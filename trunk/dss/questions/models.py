a93112d9500895d87325608e4e28a6e712890e7e
adriankags@gmail.com <adriankags@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-02-13 01:54:59 +0000

    created guest model for supporting guest users

--- trunk/dss/questions/models.py
+++ trunk/dss/questions/models.py
@@ -28,3 +28,25 @@ class Recommendation(models.Model):
     recommendation = models.TextField()
     def __unicode__(self):
         return self.recommendation
+
+class Guest(models.Model):
+    
+    """
+    A temporary user.
+
+    Fields:
+    ``user`` - The temporary user.
+    ``last_used`` - The last time we noted this user doing something.
+    
+    All users with a record in this model are temporary and should be
+    deleted after GUEST_DELETE_TIME.
+    
+    """
+
+    user = models.ForeignKey(User)
+    last_used = models.DateTimeField(User)
+
+    @classmethod
+    def create_guest(self, user):
+        guest = Guest(user=user, last_used=datetime.datetime.now())
+        return guest
