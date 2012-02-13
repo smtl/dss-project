cef8cb230407e8dd7421a2d5cd8df54e359c34b8
adriankags@gmail.com <adriankags@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-02-13 01:16:46 +0000

    test for support on windows based machines

--- trunk/dss/questions/tests.py
+++ trunk/dss/questions/tests.py
@@ -288,3 +288,20 @@ class CsrfViewMiddlewareTest(TestCase):
         req2 = CsrfViewMiddleware().process_view(req, post_form_view, (), {})
         self.assertEqual(None, req2)
 
+class MyTestCase(unittest.TestCase):
+
+    @unittest.skip("demonstrating skipping")
+    def test_nothing(self):
+        self.fail("shouldn't happen")
+
+    @unittest.skipIf(mylib.__version__ < (1, 3),
+                     "not supported in this library version")
+    def test_format(self):
+        # Tests that work for only a certain version of the library.
+        pass
+
+    @unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
+    def test_windows_support(self):
+        # windows specific testing code
+        pass
+
