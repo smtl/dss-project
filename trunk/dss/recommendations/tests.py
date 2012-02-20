566e8e31709ebcd59577aa1ef11164c337b9a7c3
smurphs33@gmail.com <smurphs33@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-02-20 00:20:39 +0000

    test for recommendations

--- trunk/dss/recommendations/tests.py
+++ trunk/dss/recommendations/tests.py
@@ -4,7 +4,7 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from dss.recommendations.models import Recommendation
 from django.test import TestCase
 
 
@@ -14,3 +14,12 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+#Stephen Murphy
+#test if recommendations are stored and retrieved correctly
+class RecommendationsTest(TestCase):
+    def setUp(self):
+        self.rec = Recommendation.objects.create(recommendation="this")
+        
+    def test_presence(self):
+        self.assertEqual(Recommendation.objects.all().get().recommendation,str(self.rec))
