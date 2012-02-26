74d2f78d7ba2d4a7b70331444f3b79b6ecda175c
smurphs33@gmail.com <smurphs33@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-02-26 20:13:08 +0000

    server-side markdown instructions in recommendations page.

--- trunk/dss/recommendations/models.py
+++ trunk/dss/recommendations/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 from dss.questions.models import Answer, Question
+from django import forms
 
 # Create your models here.
+description = """
+<style type="text/css">
+#rightText{
+color:#545454;
+}
+</style>
 
+<div id="rightText">
+<b>
+<ul>
+<li>[link](http://example.com)</li>
+<li>![picture alt](/images/photo.jpeg "Title is optional")</li>
+<li># header 1 #<br/>## header 2 ##<br/> ### header 3 ###</li>
+<li>_italics_<br/>**bold**<br/>`code()`<br/></li>
+<li>1. numbered<br/>2. list<br/>3. example<br/></li>
+<li>* bullet<br/>* list<br/>* example<br/></li>
+<li>> blockquote</li>
+</ul>
+<p>for more Markdown syntax, visit <a href="http://en.wikipedia.org/wiki/Markdown">this</a> Wikipedia page.</p>
+</div>
+"""
 class Recommendation(models.Model):
-    recommendation = models.TextField()
+    recommendation = models.TextField(help_text=description)
     def __unicode__(self):
         return self.recommendation
+
 
 class RecAnswerLink(models.Model):
     question = models.ForeignKey(Question)
