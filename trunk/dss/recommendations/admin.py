9329dc6f23547343f748437d1578bab655a0f38e
adriankags@gmail.com <adriankags@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-03-01 20:04:38 +0000

    Maintainer can now link recommendations to profiles

--- trunk/dss/recommendations/admin.py
+++ trunk/dss/recommendations/admin.py
@@ -1,10 +1,22 @@
-from dss.recommendations.models import Recommendation
+from dss.recommendations.models import Recommendation, RecommendationProfile
 from django.contrib import admin
+
+
+class RecommendationProfileInline(admin.TabularInline):
+    model = RecommendationProfile
+    extra = 0
+    fieldsets = [
+        (RecommendationProfile, {
+            'fields': ('recommendation', 'profile',)
+        })
+    ]
 
 class RecommendationAdmin(admin.ModelAdmin):
     fieldsets = [
        ('Recommendation', {'fields': ['recommendation']}),
     ]
+    inlines = [RecommendationProfileInline] 
     search_fields = ['recommendation']
+
 
 admin.site.register(Recommendation, RecommendationAdmin)
