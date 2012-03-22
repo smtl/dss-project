992610a1b2b339c3095ccbc28e1bf311ded5eeac
stephenlowry@gmail.com <stephenlowry@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-03-22 01:55:02 +0000

    Removed uneccessary file

--- trunk/dss/questions/decorator.py
+++
@@ -1,7 +0,0 @@
-from django.utils.decorators import decorator_from_middleware
-
-from signedcookies.middleware import SignedCookiesMiddleware
-
-signed_cookies = decorator_from_middleware(SignedCookiesMiddleware)
-
-
