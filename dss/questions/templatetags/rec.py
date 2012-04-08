bb8b6a4bbf0a25a9725065cd9258f008bdaea5ea
stephenlowry@gmail.com <stephenlowry@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-04-08 23:49:03 +0000

    rules backend working for guests too

--- dss/questions/templatetags/rec.py
+++ dss/questions/templatetags/rec.py
@@ -7,9 +7,116 @@ from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 import os
 import glob
+import re
+from questions.models import Question, Answer, AnsweredQuestion
+from recommendations.models import Recommendation, UserRecommendation
 
 register = Library()
 
+def parse_rule(rule, context):
+    print "in parse rule"
+    if 'request' in context:
+        print "got request"
+        request = context['request']
+    # rule is a string
+    # "if 2 or 3 and 5 then r7"
+    bool_rule = re.compile(":").split(rule)[0]
+    result_part = re.compile(":").split(rule)[1]
+    print "This is the bool_rule: "+bool_rule
+    tokens = bool_rule.split(' ')
+    result_tokens = result_part.split(' ')
+    new_rule = bool_rule
+    current_user = request.user
+    # create the logic string
+    for t in tokens:
+        if 'ans' in t:
+            current_answer = get_or_none(Answer, id=int(float(t[3:])))
+            # Handle users and guests differently
+            if request.user.is_authenticated():
+                ans_bool_result = get_or_none(AnsweredQuestion, user=current_user, answer=current_answer)
+            elif current_answer.question in request.session:
+                ans_bool_result = "question found"
+            else:
+                ans_bool_result = None
+
+            if ans_bool_result == None:
+                ans_bool = False
+            else:
+                ans_bool = True
+            # create the new rule which is to be executed
+            new_rule = new_rule.replace(t, str(ans_bool))
+    
+    # Build and execute the rule putting the result in the "result" variable
+    result_string = "result = "+new_rule
+    exec(result_string)
+    if result == True:
+        # Actions for the outcomes of a rule
+        for t in result_tokens:
+            # red denotes redundancy
+            if "red" in t:
+                print "outcome results in a question being made redundant"
+                question = get_or_none(Question, id=int(float(t[3:])))
+                # check if question has already been answered, if it has there is no point marking it redundant
+                # actually this is a design decision - it can be changed if neccesary
+                if current_user.is_authenticated():
+                    answered = get_or_none(AnsweredQuestion, user=current_user, question=question)
+                elif question in request.session:
+                    answered = "question found"
+                else:
+                    answered = None
+
+                if answered == None:
+                    if current_user.is_authenticated():
+                        aq = AnsweredQuestion()
+                        aq.user = current_user
+                        aq.question = question
+                        aq.answer_id = 0
+                        aq.redundancy = 1
+                        aq.save()
+                    else:
+                        # redundancy marked by r at the start of the question
+                        request.session["r"+question] = 0
+            # rec denotes recommendation
+            elif "rec" in t:
+                print "outcome results in a recommendation being recommended"
+                rec = get_or_none(Recommendation, id=int(float(t[3:])))
+                # check if recommendation is already recommended for user or guest
+                if current_user.is_authenticated():
+                    recommended = get_or_none(UserRecommendation, user=current_user, recommendation=rec)
+                elif rec in request.session:
+                    recommended = request.session[rec]
+                if recommended == None:
+                    if current_user.is_authenticated():
+                        ur = UserRecommendation()
+                        ur.user = current_user
+                        ur.recommendation = rec
+                        ur.save()
+                    else:
+                        request.session[rec] = rec
+            # answer denotes to answer a question implictly
+            elif "ans" in t:
+                print "outcome results in marking a answer as implicitly answered"
+                # check if question is already answered by user. If it is, there is no need to mark it implicit
+                answer = get_or_none(Answer, id=int(float(t[3:])))
+                if current_user.is_authenticated():
+                    answered = get_or_none(AnsweredQuestion, user=current_user, question=answer.question)
+                elif answer.question in request.session:
+                    answered = "Not None"
+                else:
+                    answered = None
+
+                if answered == None:
+                    if current_user.is_authenticated():
+                        aq = AnsweredQuestion()
+                        aq.user = current_user
+                        aq.question = answer.question
+                        aq.answer = answer
+                        aq.implicit = 1
+                        aq.save()
+                    else:
+                        request.session["i"+answer.question] = answer
+    else:
+        print "false lol"
 
 def get_or_none(model, **kwargs):
     try:
@@ -22,7 +129,6 @@ def build_rec_list(parser,token):
     {% get_rec_list %}
     """
     return RecObj()
-   
 
 class RecObj(Node):
     def render(self,context):
@@ -31,7 +137,9 @@ class RecObj(Node):
         # Get the page request so we know who the user is
         if 'request' in context:
             request = context['request']
-        
+        print "test" 
+        parse_rule("ans1 and ans3 : rec13", context)
+
         # If they are signed in we query the database
         if request.user.is_authenticated():
             rec_answers = []
