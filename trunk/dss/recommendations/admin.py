f954eb2b56a600fb86a781768d454f6e32a48141
stephenlowry@gmail.com <stephenlowry@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-04-12 20:31:26 +0000

    rules page added

--- trunk/dss/recommendations/admin.py
+++ trunk/dss/recommendations/admin.py
@@ -1,5 +1,10 @@
-from recommendations.models import Recommendation, RecommendationProfile, UploadedFile, RecAnswerLink
+from recommendations.models import Recommendation, RecommendationProfile, UploadedFile, RecAnswerLink, Rule
+from questions.models import Question, Answer
 from django.contrib import admin
+from django.conf.urls.defaults import *
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.admin.views.decorators import staff_member_required
 
 class RecommendationProfileInline(admin.TabularInline):
     model = RecommendationProfile
@@ -9,7 +14,6 @@ class RecommendationProfileInline(admin.TabularInline):
             'fields': ('recommendation', 'profile',)
         })
     ]
-
 
 class RecommendationAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -48,9 +52,60 @@ class MultipleFilesAdmin(admin.ModelAdmin):
     ]
     search_fields = ["recommendation"]
 
+class RuleAdmin(admin.ModelAdmin):
+    def rules(self, request):
+        return admin_rules(request, self)
 
+    def get_urls(self):
+        urls = super(RuleAdmin, self).get_urls()
+        my_urls = patterns('', (r'^rules/$', self.rules))
+        return my_urls + urls
 
-admin.site.register(Recommendation, RecommendationAdmin)
-admin.site.register(UploadedFile, UploadedFileAdmin)
+@staff_member_required
+def admin_rules(request, model_admin):
+    opts = model_admin.model._meta
+    admin_site = model_admin.admin_site
+    has_perm = request.user.has_perm(opts.app_label+'.'+opts.get_change_permission())
+    
+    # To show rules already in system
+    rule_list = []
+    for ru in Rule.objects.all():
+        rule_list.append(ru)
+
+    # To display answers to choose from
+    answer_list = []
+    for a in Answer.objects.all():
+        answer_list.append(a)
+    
+    # To display recommendations to choose from
+    rec_list = []
+    for r in Recommendation.objects.all():
+        rec_list.append(r)
+
+    # To display questions to choose from (to make redundant)
+    question_list = []
+    for q in Question.objects.all():
+        question_list.append(q)
+
+    # To choose bool operator to choose from
+    bool_list = ["and", "or"]
+    
+    context = {'admin_site': admin_site.name,
+                'title': "Create Custom Rules",
+                'opts': opts,
+                'root_path': '/%s' % admin_site.root_path,
+                'app_label': opts.app_label,
+                'has_change_permission': has_perm,
+                'question_list': question_list,
+                'rec_list': rec_list,
+                'rule_list': rule_list,
+                'answer_list': answer_list,
+                'bool_list': bool_list
+                }
+    template = 'admin/recommendations/recommendation/rules.html'
+    return render_to_response(template, context, context_instance=RequestContext(request))
+
+admin.site.register(Recommendation, RuleAdmin)
+#admin.site.register(Recommendation, RecommendationAdmin)
+#admin.site.register(UploadedFile, UploadedFileAdmin)
 #admin.site.register(RecAnswerLink, RecAnswerLinkAdmin)
-
