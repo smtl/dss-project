95683658b6f9bd7fef1b536f774ba23457f171fd
stephenlowry@gmail.com <stephenlowry@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-04-14 21:20:38 +0000

    Maintainer can now see rules in the system

--- dss/rules/admin.py
+++ dss/rules/admin.py
@@ -9,6 +9,13 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
+import re
+
+def get_or_none(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        return None
 
 class RuleAdmin(admin.ModelAdmin):
     def rules(self, request):
@@ -27,23 +34,33 @@ def admin_rules(request, model_admin=None):
         boolpart = request.POST['boolpart']
         part2 = request.POST['part2']
 
-        if not part2:
-            boolstring = "ans"+part1
-        if boolpart != "None":
-            if part2:
-                boolstring = "ans"+part1+" "+boolpart+" "+"ans"+part2
-        
+        # build up the boolean part of the rule
+        boolstring = ""
+        if part1:
+            boolstring += "ans"+part1
+            if boolpart != "None":
+                if part2:
+                    boolstring += " "+boolpart+" ans"+part2
+        print boolstring
+
         # results part
         rec_result = request.POST['result1']
         ans_result = request.POST['result2']
         red_result = request.POST['result3']
         resultstring = ""
+        
+        if rec_result:
+            rec_result="rec"+rec_result
+        if ans_result:
+            ans_result="ans"+ans_result
+        if red_result:
+            red_result="red"+red_result
+
         temp_result_list = [rec_result, ans_result, red_result]
         for r in temp_result_list:
-            if r != "None":
                 resultstring = resultstring+r+" "
         
-        if ("None" not in resultstring) and (len(resultstring) > 3):
+        if len(boolstring) > 1 and (len(resultstring) > 1):
             rule = Rule()
             rule.rule = boolstring+" : "+resultstring
             rule.save()
@@ -57,7 +74,40 @@ def admin_rules(request, model_admin=None):
     # To show rules already in system
     rule_list = []
     for ru in Rule.objects.all():
-        rule_list.append(ru)
+        rule = ""
+        result_str = ""
+        bool_rule = re.compile(":").split(ru.rule)[0]
+        result_part = re.compile(":").split(ru.rule)[1]
+        print result_part
+        tokens = bool_rule.split(' ')
+        result_tokens = result_part.split(" ")
+        for t in tokens:
+            if "ans" in t:
+                temp_ans = get_or_none(Answer, pk=(int(float(t[3:]))))
+                if temp_ans != None:
+                    rule += str(temp_ans)
+            if t == "and" or t == "or":
+                rule += " <strong>"+t+"</strong> "
+        
+        result_str += "<br /><strong>triggers the following:</strong><ul> "
+        for rt in result_tokens:
+            if "rec" in rt:
+                temp_rec = get_or_none(Recommendation, pk=(int(float(rt[3:]))))
+                if temp_rec != None:
+                    result_str += "<li>A recommendation: "+str(temp_rec)+"</li>"
+            
+            if "ans" in rt:
+                temp_ans_result = get_or_none(Answer, pk=(int(float(rt[3:]))))
+                if temp_ans_result != None:
+                    result_str += "<li>A question being implicitly answered: "+str(temp_ans)+"</li>"
+            
+            if "red" in rt:
+                temp_red = get_or_none(Question, pk=(int(float(rt[3:]))))
+                if temp_red != None:
+                    result_str += "<li>A question being made redundant: "+str(temp_red)+"</li>"
+
+
+        rule_list.append(rule+result_str+"</ul>")
     
     # To display answers to choose from
     answer_list = []
