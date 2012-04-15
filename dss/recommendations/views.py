0c1545c57cbdbbba02e55af8672b783456c9e48e
adriankags@gmail.com <adriankags@gmail.com@ec660e9e-8ee7-1ab4-7334-92822c00b74b>
2012-04-15 10:49:25 +0000

    enhanced view

--- dss/recommendations/views.py
+++ dss/recommendations/views.py
@@ -1,4 +1,4 @@
-from recommendations.models import Recommendation, RecAnswerLink, UploadedFile
+from recommendations.models import Recommendation, RecAnswerLink, UploadedFile, Slide
 from questions.models import Question, Answer, AnsweredQuestion
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
